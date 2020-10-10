ebf1baf58d894888c4bfe0a228695441031bb7d6
SarahK05 <72619184+SarahK05@users.noreply.github.com>
2020-10-10 12:18:57 +0200

    Create stringverarbeitung.dart

---
+++ stringverarbeitung.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Sarah";
+  print("Mein Name ist $name");//Interpolation
+  double a = 1.1;
+  String b = a.toString();//Casting
+  print(b);
+  int c = name.length;
+  print("$name hat $c Buchstaben");
+}
